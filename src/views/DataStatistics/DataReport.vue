46a932c71df2e53c628c50a3b87a5e5725214778
codesky188 <703616834@qq.com>
2020-11-13 04:42:56 +0800

    数据报表

--- src/views/DataStatistics/DataReport.vue
+++ src/views/DataStatistics/DataReport.vue
@@ -1,28 +1,70 @@
 <template>
-<div class=''>
-    数据报表
-</div>
+  <div class="">
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>数据统计</el-breadcrumb-item>
+      <el-breadcrumb-item>数据报表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card>
+      <div id="main" style="width: 600px;height:400px;"></div>
+    </el-card>
+  </div>
 </template>
 
 <script>
+import echarts from 'echarts'
+import _ from 'lodash'
 export default {
   components: {},
   data() {
     return {
+      options: {
+        title: {
+          text: '用户来源'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#E9EEF3'
+            }
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ]
+      }
     }
   },
   created() {
-
   },
-  mounted() {
-
+  async mounted() {
+    var myChart = echarts.init(document.getElementById('main'))
+    const { data: res } = await this.$http.get('reports/type/1')
+    if (res.meta.status !== 200) {
+      return this.$message.error('获取折现图数据失败！')
+    }
+    const result = _.merge(res.data, this.options)
+    myChart.setOption(result)
   },
   methods: {
-
   }
 }
 </script>
-<style lang='less' scoped>
+<style lang="less" scoped>
 //@import url(); 引入公共css类
-
 </style>
